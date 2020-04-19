bcc3937ed60caa4b10e68b4ab3162c040acb14bc
github username <josephspinelli@yahoo.com>
2020-04-19 16:29:16 -0500

    building out foundation for stage and game dimensions

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Cell from './Cell';
 
 const Stage = ({ stage }) => (
-  <div><Cell /></div>
+  <div>
+    {/* mapping through stage array where each row is also an array of cells. For each sell we want
+      to render out a cell component with given arguments.  */}
+    {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
+  </div>
 );
 
 // can verify props with propTypes
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createStage } from '../gameHelpers';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
@@ -8,7 +9,7 @@ import StartButton from './StartButton';
 const Tetris = () => {
   return (
     <div>
-      <Stage />
+      <Stage stage={createStage()}/>
       {/* side bar element */}
       <aside>
         <div>
