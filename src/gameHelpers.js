1a4885254a71330e1902973232397e7590fd49e9
github username <josephspinelli@yahoo.com>
2020-05-10 23:59:23 -0500

    comments

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -6,11 +6,11 @@ const dimArr = [0, 'clear']
 
 // here filling up the stage (20 cell's high and 12 cell's wide). Individual cell in a row
 // fill up with [0, 'clear'], 0 representing a clean cell and 'clear' means no tetromino in the cell.
-
 export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, 'clear'])
   )
+// line 11
 // The Array.from() method creates a new, shallow-copied Array instance from an array-like or iterable object.
 // Example:
 // console.log(Array.from('foo'));
